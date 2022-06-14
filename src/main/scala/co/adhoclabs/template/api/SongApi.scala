977005465a8f011356c202f0cf75f07a2090e9c0
Alexan Chaprazian <alexan@adhoclabs.co>
2022-06-14 14:21:20 -0400

    Updated SongApi and SongManager
    Added a delete endpoint to allow a user to delete
    a single song. Given a UUID the instances if the song that correlates
    with it will be deleted.

--- src/main/scala/co/adhoclabs/template/api/SongApi.scala
+++ src/main/scala/co/adhoclabs/template/api/SongApi.scala
@@ -1,19 +1,22 @@
 package co.adhoclabs.template.api
 
+import java.util.UUID
+
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import co.adhoclabs.template.business.SongManager
 import co.adhoclabs.template.models.{CreateSongRequest, Song}
-import java.util.UUID
 import org.slf4j.{Logger, LoggerFactory}
+
+import scala.concurrent.ExecutionContext
 
 trait SongApi extends ApiBase {
   val routes: Route
 }
 
-class SongApiImpl(implicit songManager: SongManager) extends SongApi {
+class SongApiImpl(implicit songManager: SongManager, executionContext: ExecutionContext) extends SongApi {
 
   val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
@@ -31,6 +34,9 @@ class SongApiImpl(implicit songManager: SongManager) extends SongApi {
           },
           put {
             putSong(id)
+          },
+          delete { // Added delete endpoint
+            deleteSong(id)
           }
         )
       }
@@ -57,4 +63,11 @@ class SongApiImpl(implicit songManager: SongManager) extends SongApi {
       }
     }
   }
+
+  //added definition of delete to work with songManager class and methods
+  def deleteSong(id: UUID): Route =
+    complete {
+      songManager.delete(id).map(_ => "Song Deleted.")
+    }
 }
+
