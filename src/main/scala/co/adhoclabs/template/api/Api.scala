280a60d974a8f82a0ef9d098add28a3e6d215352
Bill Frasure <bill@adhoclabs.co>
2024-01-19 16:00:52 -0700

    Figure out Akka->ZioHttp routing :D :D

--- src/main/scala/co/adhoclabs/template/api/Api.scala
+++ src/main/scala/co/adhoclabs/template/api/Api.scala
@@ -2,17 +2,19 @@ package co.adhoclabs.template.api
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
+import akka.http.scaladsl.model.{HttpEntity, HttpRequest, HttpResponse, StatusCodes, UniversalEntity}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.RouteResult.{Complete, Rejected}
 import akka.http.scaladsl.server.directives.LoggingMagnet
-import akka.http.scaladsl.server.{ExceptionHandler, Rejection, Route, RouteResult}
+import akka.http.scaladsl.server.{ExceptionHandler, Rejection, RequestContext, Route, RouteResult}
 import akka.util.ByteString
 import co.adhoclabs.template.business.{AlbumManager, HealthManager, SongManager}
 import co.adhoclabs.template.exceptions.{UnexpectedException, ValidationException}
 import org.slf4j.{Logger, LoggerFactory}
+import zio.{Unsafe, ZIO}
+import zio.http.{Body, Header, Headers, Method, Request, URL}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 
 trait Api extends ApiBase
@@ -32,6 +34,26 @@ class ApiImpl(
   val songApi: SongApi = new SongApiImpl
   val albumApi: AlbumApi = new AlbumApiImpl
 
+  private def passUnhandledRequestsOverToZioHttp(req: RequestContext): Future[RouteResult] =
+    for {
+      zioRequest <- {
+        println("\nUnrecognized akka request: " + req.request + "\n")
+        akkaToZio(req.request)
+      }
+      response <- {
+        println("\nconverted zio     request: " + zioRequest + "\n")
+
+        val routeZio = HealthRoute.routes.apply(zioRequest)
+        val runtime = zio.Runtime.default
+        Unsafe.unsafe { implicit unsafe =>
+          runtime.unsafe.runToFuture(routeZio.mapError(errorResponse => throw new Exception(errorResponse.body.toString)))
+        }
+        //        Future.successful()
+      }
+    } yield {
+      RouteResult.Complete(HttpResponse.apply(StatusCodes.OK, entity = response.body.toString))
+    }
+
   override val routes: Route = {
     concat(
       healthApi.routes,
@@ -39,8 +61,45 @@ class ApiImpl(
         handleExceptions(exceptionHandler) {
           songApi.routes ~ albumApi.routes
         }
-      }
+      },
+      passUnhandledRequestsOverToZioHttp
     )
+  }
+
+  private def convertEntity(entity: HttpEntity): Future[Array[Byte]] = {
+    import scala.concurrent.duration._
+    import akka.http.scaladsl.model.HttpEntity
+    import akka.util.ByteString
+    import scala.concurrent.Future
+    import akka.http.scaladsl.unmarshalling.Unmarshal
+    import akka.stream.Materializer
+    import scala.concurrent.ExecutionContext
+    //      import scala.concurrent.ExecutionContext.Implicits.global
+
+    def entityToBytes()(implicit materializer: Materializer): Future[Array[Byte]] = {
+      Unmarshal(entity).to[ByteString].map(_.toArray)
+    }
+    entityToBytes()
+  }
+
+  private def akkaToZio(akkaRequest: HttpRequest): Future[Request] = {
+    val headers = {
+      Headers.apply(
+        akkaRequest.headers.map { header =>
+          Header.Custom(header.name(), header.value())
+        }
+      )
+    }
+
+    convertEntity(akkaRequest.entity) map { entityBytes =>
+      Request(
+        method  = Method.fromString(akkaRequest.method.name),
+        url     = URL.decode(akkaRequest.uri.toString()).getOrElse(???),
+        headers = headers,
+        body    = Body.fromArray(entityBytes)
+      )
+    }
+
   }
 
   private def logRequestResponse(request: HttpRequest, response: HttpResponse): Unit = {
