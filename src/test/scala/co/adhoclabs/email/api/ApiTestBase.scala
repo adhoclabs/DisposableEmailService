3b42647bfbb8a2828248f67ed657e471b2060d69
Bill Frasure <bill@adhoclabs.co>
2024-04-16 11:39:55 -0500

    Get tests passing

--- src/test/scala/co/adhoclabs/email/api/ApiTestBase.scala
+++ src/test/scala/co/adhoclabs/email/api/ApiTestBase.scala
@@ -1,18 +1,16 @@
 package co.adhoclabs.email.api
 
 import co.adhoclabs.email.TestBase
-import co.adhoclabs.email.api.{AlbumRoutes, ApiZ, HealthRoutes, SongRoutes}
-import co.adhoclabs.email.business.{AlbumManager, HealthManager, SongManager}
+import co.adhoclabs.email.api.{ApiZ, HealthRoutes}
+import co.adhoclabs.email.business.{EmailManager, HealthManager}
 
 abstract class ApiTestBase extends TestBase with ZioHttpTestHelpers {
 
   implicit val healthManager: HealthManager = mock[HealthManager]
-  implicit val songManager: SongManager     = mock[SongManager]
-  implicit val albumManager: AlbumManager   = mock[AlbumManager]
+  implicit val emailManager: EmailManager   = mock[EmailManager]
 
   // ZIO-http bits
-  implicit val albumbRoutes = AlbumRoutes()
-  implicit val songRoutes   = SongRoutes()
+  implicit val albumbRoutes = EmailRoutes()
   implicit val healthRoutes = HealthRoutes()
 
   val zioRoutes = ApiZ().zioRoutes
